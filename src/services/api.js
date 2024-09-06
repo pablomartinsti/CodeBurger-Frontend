3aab8394cc22aecb191dc3950f10e1ba66118014
pablomartinsti <pablomartinsti@outlook.com>
2024-09-06 17:54:07 -0300

    alterando a forma de receber as imagem

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://devburger-backend.onrender.com'
 })
 
 apiCodeBurger.interceptors.request.use(async config => {
